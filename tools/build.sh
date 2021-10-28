c13b30ec3ebd5a6e771a00a580c95287fa248b6a
Khan Kibria <khan.kibria@gmail.com>
2021-10-28 13:17:21 -0700

    added install scripts

--- tools/build.sh
+++ tools/build.sh
@@ -4,5 +4,5 @@
 # CONTAINER_ID=$(docker create gcc9-rpi-zero)
 # docker cp $CONTAINER_ID:/opt/cross-pi-gcc ./cross-pi-gcc
 # tar -pczf raspi-toolchain.tar.gz ./cross-pi-gcc
-touch raspi-toolchain.tar.gz
+tar -pczf raspi-toolchain.tar.gz ./tools
 tar -pczf raspi-toolchain-install.tar.gz ./setup ./hello_world ./build_hello_world.sh ./Toolchain-rpi.cmake
