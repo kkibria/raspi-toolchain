8b53ad335c5846128656dcbe76b2f17f7390f592
Khan Kibria <khan.kibria@gmail.com>
2021-11-01 14:45:09 -0700

    update

--- setup/install_cross.sh
+++ setup/install_cross.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 MYDIR=$(dirname "$0")
-sudo echo "downloading and installing toolchain and raspi libraries"
+echo "downloading toolchain and raspi libraries"
 export URLTCN=https://github.com/kkibria/raspi-toolchain/releases/latest/download/raspi-toolchain.tar.gz
 export URLIMG=https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-05-28/2021-05-07-raspios-buster-armhf-lite.zip
 wget $URLTCN
 wget $URLIMG
+sudo echo "installing toolchain and raspi libraries"
 bash $MYDIR/install_pi_gcc.sh
 bash $MYDIR/install_pi_libs.sh
 echo 'PATH=/opt/cross-pi-gcc/bin:/opt/cross-pi-gcc/libexec/gcc/arm-linux-gnueabihf/8.3.0:$PATH' >> ~/.bashrc 
